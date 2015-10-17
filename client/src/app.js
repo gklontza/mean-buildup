977b4b661b9cb4cacfa9d70859102b8598dbbf95
gklontza <gklontza@gmail.com>
2015-10-17 14:02:08 +0100

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -5,4 +5,11 @@ angular
     .module('angular-buildup', [])
     .run(function() {
         console.log('Angular app have runned');
+    .config(function($routeProvider) {
+        $routeProvider
+
+            .when('/', {
+                templateUrl : '/static/src/index.html'
+            })
+
     });
